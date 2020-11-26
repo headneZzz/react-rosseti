1c3be15f29bf659d84a0d93587ed4766520feb80
Your Name <shturmtm1@mgmail.com>
2020-11-26 21:26:10 +0300

    header menu v2

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,22 +7,41 @@ export default (props) => {
     const {Header} = Layout;
     return (
         <Header style={{ zIndex: 1, width: '100%', backgroundColor: "white", height: "auto" }}>
-                <Image src={"https://www.b2b-mrsk.ru/images/gates/rus/mrsk.png"} width={160} style=
+                <Image src={"https://www.b2b-mrsk.ru/images/gates/rus/mrsk.png"} width={160} height={50} style=
                     {{
-                        margin: 16,
+                        margin: 15,
                         float: "left"
                     }}/>
-                <Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']} style=
+                <Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']}  height={50} style=
                     {{
-                        margin: 16,
+                        margin: 15,
                         float: "left"
                     }}>
                     <Menu.Item key="1" className="modified-item">Проблемы</Menu.Item>
                     <Menu.Item key="2" className="modified-item">Отчеты</Menu.Item>
                     <Menu.Item key="3" className="modified-item">Проекты</Menu.Item>
                 </Menu>
+                <Profile name={"Дора Дура"} photo={"https://images.genius.com/3a3d16332164a48f9e46f160cb8cd6fa.300x300x1.jpg"}/>
         </Header>
     )
+}
+
+
+function Profile(props) {
+    return (
+        <div className={"profile-container"} style={{display: "flex", float: "right"}}>
+                    <div style={{margin: 15, }}>
+                        <span style={{fontWeight: 700, fontSize: 16}}>
+                            {props.name}
+                        </span>
+                    </div>
+                    <Image src={props.photo} width={50} height={50} style=
+                        {{
+                            margin: 15,
+                            float: "right",
+                        }}/>
+                </div>
+    );
 }
 
 
