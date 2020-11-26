16db9912148764d10776411ee9460ff9e6ea3173
Your Name <shturmtm1@mgmail.com>
2020-11-27 00:21:38 +0300

    header menu v5

---
+++ src/components/Form.js
@@ -0,0 +1,116 @@
+import {Button, Layout, PageHeader, Spin, Row, Col, Table, Tag, Space, Input, Select} from 'antd';
+import {PlusOutlined} from "@ant-design/icons";
+import React from "react";
+import {Link} from "react-router-dom";  
+import './Header.css'
+
+const columns = [
+    {
+      title: 'Название',
+      dataIndex: 'name',
+      key: 'name',
+      render: text => <a>{text}</a>,
+    },
+    {
+        title: 'Категория',
+        key: 'category',
+        dataIndex: 'category',
+        render: category => (
+          <>
+            {category.map(tag => {
+              let color = tag.length > 5 ? 'geekblue' : 'green';
+              if (tag === 'loser') {
+                color = 'volcano';
+              }
+              return (
+                <Tag color={color} key={tag}>
+                  {tag.toUpperCase()}
+                </Tag>
+              );
+            })}
+          </>
+        ),
+      },
+    {
+      title: 'Статус',
+      dataIndex: 'status',
+      key: 'status',
+    },
+    {
+        title: 'Автор',
+        dataIndex: 'author',
+        key: 'author',
+      },
+      {
+        title: 'Ответы',
+        dataIndex: 'replies',
+        key: 'replies',
+      },
+      {
+        title: 'Рейтинг',
+        dataIndex: 'raiting',
+        key: 'raiting',
+      },
+      {
+        title: 'Сообщения',
+        dataIndex: 'messages',
+        key: 'messages',
+      }
+  ];
+  
+  const data = [
+    {
+      key: '1',
+      name: 'Как какать',
+      status: 32,
+      author: 'New York No. 1 Lake Park',
+      replies : "23",
+      raiting : "200",
+      messages : "23",
+      category: ['nice'],
+    },
+    {
+        key: '2',
+        name: 'Как какать',
+        status: 32,
+        author: 'New York No. 1 Lake Park',
+        replies : "23",
+        raiting : "200",
+        messages : "23",
+        category: ['developer'],
+      }
+  ];
+
+  const { Search } = Input;
+  const onSearch = value => console.log(value);
+export default (props) => {
+    return (
+        <div>
+        <Row style={{marginBottom: 10,
+        marginTop: 10}}>
+                            <Col span={4}>
+                            <Search placeholder="Поиск..." onSearch={()=>{}} size={"middle"} />
+                            </Col>
+                            <Col span={1}></Col>
+                            <Col span={7} style={{height: 40}}>
+                              <div style={{display: "flex", alignItems: "center", flexDirection : "row"}}>
+                                <div style={{fontFamily: "Roboto", fontWeight: 500, fontSize: 16, color: "#929292", paddingRight: 20}}>
+                                  Сначала:
+                                </div>  
+                                <Select size={"middle"} defaultValue="Популярные" style={{ width: 150 }}>
+                                  <Select value="Новые">Новые</Select>
+                                  <Select value="Популярные">Популярные</Select>
+                                </Select>
+                              </div>
+                              
+                            </Col>
+                            <Col span={12}>
+                              <Button type="primary" shape="round" icon={<PlusOutlined />} size={"middle"} color={"#008A60"} style={{float: 'right'}}>
+                                Добавить инициативу
+                              </Button>
+                            </Col>
+                        </Row>
+                        <Table columns={columns} dataSource={data} />
+                        </div>
+    )
+}
--- src/pages/initiatives/index.js
+++ src/pages/initiatives/index.js
@@ -1,35 +1,25 @@
 import React, {useState} from 'react'
-import {Button, Layout, PageHeader, Spin, Row, Col} from 'antd';
+import {Button, Layout, PageHeader, Spin, Row, Col, Table, Tag, Space, Input} from 'antd';
 import Header from "../../components/Header";
 import Title from "../../components/Titile"
+import Form from "../../components/Form"
+
 export default function Initiatives() {
     const [isLoading, setLoading] = useState(true);
     const {Content} = Layout;
     return (
-        <Layout className="wrapper">
+        <Layout className="wrapper" style={{backgroundColor: 'white'}}>
             <Header selected={"stocktaking"}/>
             <Title title={"Инициативы"} description={"Здесь список всех рационализаторских инициатив"}/>
-            <Layout>
-                <Layout style={{marginLeft: 25, marginRight: 25, marginTop: 25}}>
-                    <PageHeader
-                        ghost={false}
-                        title="Рационализаторские инициативы"
-                        extra={[
-                                <Button key="1" type="primary">
-                                    Добавить инициативу
-                                </Button>
-                        ]}
-                    />
-
+            <Layout style={{backgroundColor: "white"}}>
+                <Layout style={{marginLeft: 50, marginRight: 50, marginTop: 25, backgroundColor: 'white'}}>
                     <Content className="site-layout-background" style={{
                         padding: '0 24px',
-                        overflow: 'auto'
+                        overflow: 'auto',
+                        backgroundColor: 'white'
                     }}>
-                        {isLoading ?
-                            <div style={{textAlign: "center", paddingTop: 200}}><Spin size="large"/></div>
-                            :
-                            <></>
-                        }
+                        
+                        <Form />
                     </Content>
                 </Layout>
             </Layout>
