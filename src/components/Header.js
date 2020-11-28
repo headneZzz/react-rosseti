1d3beca167e7681d4c0976cc3012b1c99a752baa
Your Name <shturmtm1@mgmail.com>
2020-11-28 17:19:39 +0300

    routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,22 @@
-import {Layout, Menu, Image} from "antd";
-import {AppstoreOutlined, TableOutlined, DownOutlined } from "@ant-design/icons";
+import {Layout, Menu, Image, Dropdown} from "antd";
+import {AppstoreOutlined, TableOutlined, DownOutlined, CheckOutlined } from "@ant-design/icons";
 import React from "react";
 import {Link} from "react-router-dom";  
 import './Header.css'
 export default (props) => {
     const {Header} = Layout;
     return (
-        <Header style={{ zIndex: 1, width: '100%', backgroundColor: "white", height: "auto" }}>
-                <Image src={"https://www.b2b-mrsk.ru/images/gates/rus/mrsk.png"} width={160} height={50} style=
+        <Header style={{ zIndex: 1, width: '100%', backgroundColor: "white", height: "auto", display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <Link to="/main" style={{alignSelf: 'center'}}>
+                <img src={"https://www.b2b-mrsk.ru/images/gates/rus/mrsk.png"} width={160} height={50} style=
                     {{
+                        alignSelf: 'center',
+                        justifySelf: 'center',
                         margin: 15,
                         float: "left"
                     }}/>
+                </Link>
                 <Menu theme="light" mode="horizontal" defaultSelectedKeys={['2']}  height={50} style=
                     {{
                         margin: 15,
@@ -21,19 +26,41 @@ export default (props) => {
                     <Menu.Item key="2" className="modified-item">Отчеты</Menu.Item>
                     <Menu.Item key="3" className="modified-item">Проекты</Menu.Item>
                 </Menu>
-                <Profile name={"Дора Дура"} photo={"https://images.genius.com/3a3d16332164a48f9e46f160cb8cd6fa.300x300x1.jpg"}/>
+                </div>
+                <div style={{width:'20%'}}>
+                    <Profile name={"Дора Дура"} photo={"https://images.genius.com/3a3d16332164a48f9e46f160cb8cd6fa.300x300x1.jpg"}/>
+                </div>
+                
         </Header>
     )
 }
 
 
 function Profile(props) {
+    const menu = (
+        <Menu>
+          <Menu.Item key="0">
+            <Link to="/profile">
+              Профиль
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="1">
+            <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+              Выйти
+            </a>
+          </Menu.Item>
+        </Menu>
+      );
     return (
-        <div style={{display: 'flex', flexDirection: 'col', justifyContent:'center', float: 'right'}}>
+        <div>
+            <Dropdown overlay={menu}>
             <div className={"profile-container"} style={{display: "flex", float: "right", flexDirection: 'row', marginTop: 15}}>
-                    <Image src={props.photo} width={45} height={45} style=
+            
+                    <img src={props.photo} width={45} height={45} style=
                         {{
-                            float: "right"
+                            borderRadius: 100,
+                            float: "right",
+                            alignSelf: 'center'
                         }}/>
                     <div>
                         <span style={{fontSize: 16, fontFamily: "Raleway", marginRight: 10, marginLeft: 10 }}>
@@ -43,7 +70,9 @@ function Profile(props) {
                     <div>
                         <DownOutlined />
                     </div>
+                
                 </div>
+                </Dropdown>
         </div>
     );
 }
