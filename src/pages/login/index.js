bc9e9e74d575c877d55806b39967ca77e5b901c8
Ardn1 <49320695+Ardn1@users.noreply.github.com>
2020-11-29 01:01:18 +0300

    kh

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,59 +1,40 @@
 import React from "react";
-import {Form, Input, Button} from 'antd';
-import {UserOutlined, LockOutlined} from '@ant-design/icons';
-import {setUserSession} from "../../utils/sessionUtils";
+import { Form, Input, Button, Image } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { setUserSession } from "../../utils/sessionUtils";
 
 export default class LoginPage extends React.Component {
     handleSubmit = () => {
-        const user = {"name": "user"};
+        const user = { "name": "user" };
         setUserSession(user);
-        this.props.history.push('/initiativeOne')
+        this.props.history.push('/initiatives')
     };
 
     render() {
         return (
-            <div className="login-container">
-                <br/>
-                <Form
-                    name="normal_login"
-                    className="login-form"
-                    onFinish={this.handleSubmit}
-                >
-                    <h1>Россети</h1>
-
-                    <Form.Item
-                        name="name"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Введите имя',
-                            },
-                        ]}
-                    >
-                        <Input prefix={<UserOutlined className="site-form-item-icon"/>} placeholder="Имя"/>
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Введите пароль',
-                            },
-                        ]}
-                    >
+            <div className="login-container" style={{ height: "100vh", width: "100vw", display: "flex", flexDirection: "column", justifyContent: "center", justifyItems: "center", justifySelf: "center" }}>
+                <div className="login-container" style={{ height: "100vh", width: "100vw", display: "flex", flexDirection: "row", justifyContent: "center", justifyItems: "center", justifySelf: "center" }}>
+                    <div style={{ height: "10vh", width: "17vw", marginTop: "17vh", display: "flex", flexDirection: "column", justifyContent: "center", justifyItems: "center", justifySelf: "center" }}>
+                        <Image src={"/LogoRoss.png"} width={472 * 0.5} height={170 * 0.5} style=
+                            {{
+                                margin: 10,
+                                float: "right",
+                            }} />
                         <Input
-                            prefix={<LockOutlined className="site-form-item-icon"/>}
-                            type="password"
-                            placeholder="Пароль"
+                            style={{ marginTop: "15px" }} placeholder="Логин" size={'large'}
                         />
-                    </Form.Item>
-
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button">
+                        <Input
+                            style={{ marginTop: "15px" }} placeholder="Пароль" size={'large'}
+                        />
+                        <Button style={{ marginTop: "15px" }} type="primary" htmlType="submit" className="login-form-button" onClick={() => {
+                            const user = { "name": "user" };
+                            setUserSession(user);
+                            this.props.history.push('/main')
+                        }}>
                             Войти
                         </Button>
-                    </Form.Item>
-                </Form>
+                    </div>
+                </div>
             </div>
         );
     }
